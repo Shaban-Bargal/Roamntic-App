047f8df724d570ff69732a6969b000a706f8047a
bargal2 <bargalbargal2@gmail.com>
2026-07-25 00:40:08 +0300

    fix: update vite base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  base: '/Roamntic-App/',
-  
+  plugins: [react(), tailwindcss()],
+  // بيقرأ من Vercel إذا كان شغال هناك، غير كده يرجع لـ GitHub Pages
+  base: process.env.VERCEL ? '/' : '/Roamntic-App/',
 })
